fa6f9e45a6ce6a29b19b72825300bac50c1b52e6
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-07 16:02:56 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*
+ * @Author :Yitagesu Ketema
+ * */
+#include <stdio.h>
+/**
+ *  main - entry point of our programme
+ *   Return: 0
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
