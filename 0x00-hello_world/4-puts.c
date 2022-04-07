7de6f5b9ac37e23306d406260b542b20b9be6bae
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-07 15:52:03 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
 /*
- * File:4-puts.c
- * Author :Yitagesu Ketema @Alx school
- * 
+ *  @Auth :Yitagesu ketema
  */
-
 #include <stdio.h>
-/** main function that print the text
- * "Programming is like building a multilingual puzzle, followed by a new line.
- *return: always 0.
- *
-*/
+/**
+ *   main - starting point of our c programme
+ *     Return: 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle, followed by a new line");
-	return (0);
-}
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+
