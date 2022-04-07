f9259db5be3aafe7b8cff9129c779f5b1ed33d36
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-07 18:12:40 +0300

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 /*
+ * te.c
  * File: 101-quote.c
  * Auth: Yitagesu K Areda
  */
 #include <unistd.h>
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a new line, to standard error.   
+ * followed by a new line, to standard error.
  *  Return: Always 1.
  */
 int main(void)
 {
 write(1,	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",	59);
-return (1);    
+return (1);
 }
