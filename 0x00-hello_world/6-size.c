67ea204bfd08e085dfc63195f1c8abd4d92a34ba
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-07 16:48:02 +0300

    indentation added to 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,23 @@
 /*
- * @Author:Yitagesu Ketema
- */
+ * File: 6-size.c
+ * Auth: Yitagesu K Areda
+*/
+
 #include <stdio.h>
-/*
- *  main - entry point of our programme
- *   return size of major data types
- *    Return: 0
- */
-int main(void)
+
+    /*
+     * main - Prints the size of various types based on
+     *     
+     * the computer it is compiled and run on..	  	
+     * Return: Always 0.
+     */
+		int main(void)
 {
+		printf("Size of a char: %zu byte(s)\n", sizeof(char));
+			printf("Size of an int: %zu byte(s)\n", sizeof(int));
+				printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+					printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+						printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
-
-		printf("Size of a char: %i byte(s)\n", sizeof(char));
-			printf("Size of an int: %i byte(s)\n", sizeof(int));
-				printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-						printf("Size of a float: %i byte(s)\n", sizeof(float));
 							return (0);
 }
