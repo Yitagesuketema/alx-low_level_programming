5cd1fa839a3417ab58babf6151d807ba22a3f79e
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-07 18:00:32 +0300

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,6 @@
  * File: 101-quote.c
  * Auth: Yitagesu K Areda
  */
-
 #include <unistd.h>
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
@@ -11,6 +10,6 @@
  */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return (1);    
 }
