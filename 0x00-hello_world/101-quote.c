679053fbe2a7bea5cc2389520e3f87567e09aaf0
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-07 16:36:37 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,8 @@ main - main block
    /
   int main(void)
 {
-		int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+		int s= strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
-			write(2,
-						      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-						      	      s);
+			write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n",s);
 				return (1);
 }
