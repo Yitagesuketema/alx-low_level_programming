55768a2edfecd8a2c09107b069a760fe10214bf5
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-07 15:33:39 +0300

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/*
+ * File:4-puts.c
+ * Author :Yitagesu Ketema @Alx school
+ * 
+ */
+
+#include <stdio.h>
+/** main function that print the text
+ * "Programming is like building a multilingual puzzle, followed by a new line.
+ *return: always 0.
+ *
+*/
+int main(void)
+{
+	puts(\"Programming is like building a multilingual puzzle, followed by a new line");
+	return (0);
+}
