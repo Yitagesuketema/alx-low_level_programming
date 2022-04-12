df898f8e257a286e868eada5333d0edb581915c5
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-12 17:15:48 +0300

    1-alphabet.c final corr

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
-/*
+/**
  * print_alphabet - a function that prints the alphabet in lowercase
  * followed by a new line.
  * Return : void.
  */
 void print_alphabet(void)
 {
-	char c= 'a'; 
-	
-	do{
+	char c = 'a';
+	 
+	do {
 	_putchar(c);
 	c++;
-	}while (c <= 'z');
+	} while (c <= 'z');
 	_putchar('\n');
 }
