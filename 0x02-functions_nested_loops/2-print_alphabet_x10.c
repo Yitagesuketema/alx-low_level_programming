a046e9245c8c5dd5f016c871c45421c927928a16
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-12 20:31:25 +0300

    2-print_alphabet_x10.c correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet_x10 : a function that prints 10 times the alphabet, 
- * in lowercase,
+ * print_alphabet_x10 : a function that prints 10 times the alphabet. 
+ * in lowercase.
  * followed by a new line.
  * Return : void .
  */
 void print_alphabet_x10(void)
 {
-	
-	int i = 0;
+int i = 0;
 	
 	while (i <= 9)
 	{ 
