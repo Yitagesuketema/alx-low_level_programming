2053ae5897537d5377017351708dec8bfeb4f3e4
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-12 11:26:46 +0300

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+/*
+* File : 0-putchar.c
+* Auth: Yitagesu K Areda
+*/
+#include "main.h"
+/*
+*main - Writes _putchar followed by a new line.
+*Return : 0.
+*/
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
+	
+	return (0);
+
+
+}
