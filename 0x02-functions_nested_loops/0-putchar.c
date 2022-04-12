5c5a9e9e586b77050003d60c787262f041520b9d
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-12 14:41:41 +0300

    0-putchar error corrected

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,8 @@
-/*
- * File : 0-putchar.c
- * Auth: Yitagesu K Areda
- */
 #include "main.h"
 /*
  * main - prints the string  "_putchar" followed by a new line.
  * Return : 0 on success.
  */
- 
 int main(void)
 {
 	char c[] = "_putchar";
