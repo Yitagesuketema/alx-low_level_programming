43eb0957390a07f7e57992b05f20c128440574ad
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-12 16:53:58 +0300

    a function that prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+/*
+ *File : 1-alphabet.c
+ * Auth : Yitagesu K Areda
+ */
+#include "main.h"
+/*
+ * print_alphabet - a function that prints the alphabet, in lowercase, followed by a new line.
+ * Return : void.
+*/
+void print_alphabet(void)
+ {
+	char c='a'; 
+	
+	do
+	{
+	_putchar(c);
+	c++;
+	}
+	while(c <= 'z');
+ 	_putchar('\n');
+ }
