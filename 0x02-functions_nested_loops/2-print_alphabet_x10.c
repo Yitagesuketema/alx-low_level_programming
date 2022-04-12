af68e844d0fabec3d7a8a89cc44ffd0d2b69b66e
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-12 20:39:43 +0300

    2-print_alphabet_x10.c corrections

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 : print 10 times the alphabet.
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase.
  * Return : void .
  */
 void print_alphabet_x10(void)
