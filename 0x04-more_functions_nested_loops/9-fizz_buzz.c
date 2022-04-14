c5638f736ca838d85fab43ecb94f0d3dc5e5f04f
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-15 00:04:41 +0300

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,28 @@
  */
 int main(void)
 {
-	for (int i = 1; i <= 100; i++)
+	int i;
+
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
-		else
-			printf("%d ", i);
-
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
 	}
-	printf('\n');
-return (0);
-}
+	printf("\n");
 
+	return (0);
+}
