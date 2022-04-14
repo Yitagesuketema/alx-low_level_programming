349cf4166c51a7cb98c43632d59795f0ed259221
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-14 16:28:51 +0300

    print numbers  -9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- * print_numbers - print numbers from 0 to 9
- * Return: void
+ * print_numbers - prints the numbers, from 0 to 9
  */
+
 void print_numbers(void)
 {
 	int x;
