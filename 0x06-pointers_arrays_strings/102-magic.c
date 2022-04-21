63635d466008b54e913e1aeda3cd94117ca9be1c
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-21 23:03:35 +0300

    program prints a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 /**
- * main -program prints a[2] = 98
- * Return: Void
+ * main - prints a[2]=98
+ * Return: 0
  */
 int main(void)
 {
 int n;
 int a[5];
 int *p;
-
 a[2] = 1024;
 p = &n;
-*(p + 5) = 98;
+(p + 5) = 98;
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
