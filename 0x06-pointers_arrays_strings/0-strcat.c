9c69dda85b5ed1d53cb700fdf405c795ea36683e
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-21 15:48:50 +0300

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  *_strcat - concatinate 2 strings.
  *@dest: first string.
  *@src: second string.
  *Return: string.
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
@@ -15,5 +17,11 @@ char *_strcat(char *dest, char *src)
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
+		i++;
 		j++;
 	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
