5585fe2e0a45c1770c01380d2da4a673570e96f6
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-25 14:54:06 +0300

    a function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+/**
+ * _memcpy- copies memory area
+ * @dest: your name (you@domain.com)
+ * @src: a source string to be copied
+ * @n: nuber of bytes to return
+ * Return:a copied pointer string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i = 0;
+    for (i = 0; i < n; i++)
+    {
+    *(dest + i) = *(src + i);
+    }
+return (dest);
+}
