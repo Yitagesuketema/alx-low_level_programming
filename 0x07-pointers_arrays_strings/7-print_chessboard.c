a3dd1c40b3d839ef43a454216c55d138470359f7
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-25 21:20:15 +0300

    a function that prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,12 +7,11 @@
 void print_chessboard(char (*a)[8])
 {
 int i, j;
+int i, j;
 for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
-{
-printf("%c", a[i][j]);
-printf('\n');
-}
+_putchar(a[i][j]);
+_putchar('\n');
 }
 }
