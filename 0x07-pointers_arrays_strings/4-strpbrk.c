0ee50f71fdf1add440da03f438d038b0e2ed6f9c
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-25 19:25:40 +0300

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,19 +7,11 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-unsigned int i, j, success = 0;
+unsigned int i, j;
 for (i = 0; s[i] != '\0'; i++)
-{
 for (j = 0; accept[j] != '\0'; j++)
-{
 if (s[i] == accept[j])
-return (s + i);
 goto search_end;
-success = 1;
-}
 search_end:
-break;
-if (success == 0)
-return ('\0');
-}
+return (s[i] != '\0' ? s + i : '\0');
 }
