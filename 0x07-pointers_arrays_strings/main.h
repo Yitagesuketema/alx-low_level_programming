f9c9001f92cda2d74abab6a2f74e6f1236d20d21
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-26 01:02:47 +0300

    a function that sets the value of a pointer to a char

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,4 +4,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
-void set_string(char **s, char *to)
+void set_string(char **s, char *to);
