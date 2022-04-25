b7e032a5bff26c5e54bdfa7019fdad70ac124033
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-25 19:15:31 +0300

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,34 +1,25 @@
 #include "main.h"
 /**
- * @brief 
- * 
+ *_strpbrk -find the first occurence of a characture
+ * @s:a string to be searched
+ * @accept: a string to be searced by s
+ * Return:a string pointer
  */
 char *_strpbrk(char *s, char *accept)
 {
-unsigned int i,j,success=0;
-   
-for(i = 0; *(s + i)!='\0'; i++)
+unsigned int i, j, success = 0;
+for (i = 0; *(s + i) != '\0'; i++)
 {
-for (j = 0; *(accept + j) != '\0';j++)
+for (j = 0; *(accept + j) != '\0'; j++)
 {
 if (*(s + i) == *(accept + j))
-{
-return(s + i);
+return (s + i);
 goto search_end;
-success=1;
+success = 1;
 }
 search_end:
 break;
 if (success == 0)
 return ('\0');
-}
- 
-
-
-
-
-
-
-
 }
 }
