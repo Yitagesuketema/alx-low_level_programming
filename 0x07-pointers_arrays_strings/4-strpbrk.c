35f9cbe9ffdfb89031e6cf8d387287de9e113f70
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-25 19:18:59 +0300

    a function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,11 +8,11 @@
 char *_strpbrk(char *s, char *accept)
 {
 unsigned int i, j, success = 0;
-for (i = 0; *(s + i) != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; *(accept + j) != '\0'; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
-if (*(s + i) == *(accept + j))
+if (s[i] == accept[j])
 return (s + i);
 goto search_end;
 success = 1;
