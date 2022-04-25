ac837d938729a5e1791597f8e7db4ada77312cbc
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-25 18:10:30 +0300

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,10 @@
-e "main.h"
-
+#include "main.h"
 /**
  * _strchr - locates a character in a string
  * @s: string input
  * @c: character to find
  * Return: pointer to first occurence of c character
  */
-
 char *_strchr(char *s, char c)
 {
 	unsigned int i;
