c1119aeee722a6072d4a119fc2d3f869618c6c81
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-25 22:42:47 +0300

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
