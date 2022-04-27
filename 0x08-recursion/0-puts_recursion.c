8f57dcb8d685eabe42b179ada8cfec59003477b4
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-04-27 22:10:47 +0300

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,14 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
-	{
-		_putchar('\n');
-		return;
-	}
+if (*s)
+{
 	_putchar(*s);
 	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
 }
