e72d40a0c887c1197a0f65e0ad8d1b7f7d13315f
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-03 21:21:32 +0300

    free grid final

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,10 +12,10 @@
  */
 void free_grid(int **grid, int height)
 {
-    if (grid != NULL && height != 0)
-	{
-		for (; height >= 0; height--)
-			free(grid[height]);
+if (grid != NULL && height != 0)
+{
+	for (; height >= 0; height--)
+		free(grid[height]);
 		free(grid);
 	}
 }
