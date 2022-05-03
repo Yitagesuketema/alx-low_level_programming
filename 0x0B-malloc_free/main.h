61b6652aa6dec6863b3dfb41589e43ecc439baa3
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-03 22:57:41 +0300

    main.h file renewed

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,3 +3,7 @@ char *_strdup(char *str);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+int isSpace(char c);
+int startIndex(char *s, int index);
+int endIndex(char *s, int index);
+int countWords(char *s)
