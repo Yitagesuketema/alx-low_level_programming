ddb9d5e37cc31b0bdf18a22ead6a89b0b670cb40
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-03 22:49:28 +0300

    argstostr func ammended

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,11 +1,11 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * argstostr - prints args
  * @ac: takes in width of grid
  * @av: height of grid
  * Return: the args one line at a time
  */
-
 char *argstostr(int ac, char **av)
 {
 	char *str;
