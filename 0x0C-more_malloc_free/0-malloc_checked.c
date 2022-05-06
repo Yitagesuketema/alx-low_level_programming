4486ce91706a6eb58ce3e396ff474f8a46683a5a
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-06 19:33:03 +0300

    a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+/**
+ * @file 0-malloc_checked.c
+ * @author Yitagesu K Areda
+ * @date 2022-05-06
+ * @copyright Copyright (c) 2022 
+ */
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b:number of memory block. 
+ * Return: on success allocatted memory
+ * on fail normal termination with retir n 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+int *mem_alloc =malloc(sizeof(int)*b);
+    if(!mem_alloc)
+    {
+    return (98);
+    }
+return(mem_alloc);
+}
