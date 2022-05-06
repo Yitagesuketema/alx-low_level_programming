bf08456af808c36d68b1501e73d5adf67804beda
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-06 20:36:30 +0300

    readme file added

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,10 +8,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *mem = malloc(b);
+    void *mem_alloc = malloc(b);
 
-	if (mem == NULL)
-		exit(98);
+    if (!mem_alloc)
+        exit(98);
 
-	return (mem);
+    return (mem_alloc);
 }
