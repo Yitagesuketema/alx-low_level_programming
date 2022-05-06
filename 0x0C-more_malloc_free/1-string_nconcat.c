9a05674833ef590bf1630e8037cf8fc8e3b30733
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-06 21:23:14 +0300

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,42 +1,46 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * @brief
+ *string_nconcat -
  *
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    int mem_size_s2, *m;
-    if (s1 == NULL)
-        s1 = "";
-
-    if (s2 == NULL)
-        s2 = "";
-    if (n >= strlen(s2))
-    {
-        mem_size_s2 = strlen(s2);
-    }
-    else
-    {
-        mem_size_s2 = n;
-    }
-
-    *m = malloc(sizeof(char) * (strlen(s1) + mem_size_s2 + 1));
-    if (!m)
-    {
-        return (NULL);
-    }
+unsigned int i, len_s1, len_s2;
+char *concat;
+    if (!s1)
+        s1="";
+    if (!s2)
+        s2="";
+        len_s1 = strlen (*s1);
+if (n >= strlen(*s2))
+{
+len_s2 = strlen (*s2);
+concat = malloc(sizeof(char) * (len_s1 + len_s2 +1));
+}
+else 
+{
+    concat = malloc(sizeof(len_s1 +n));
+}
+for (i = 0; i < len_s1;i++)
+{
+*(concat + i) = *(s1+i);
 }
 
-int strlen(char *str)
+for (i =len_s1; i < (len_s1 + len_s2); i++)
 {
-    int i, len = 0;
-    if (str == NULL)
-        return (len);
+*(concat + i) = *(concat + i);
 
-    for (i = 0; *(str + i) != '\0'; i++)
-    {
-        len++;
-    }
-    return (len);
+}
+
+concat[len_s1 + len_s2] ='\0';
+return (concat);
+}
+
+int strlen (char *str)
+{
+    unsigned int len = 0;
+while (*str) 
+len++;
+return (len);
 }
