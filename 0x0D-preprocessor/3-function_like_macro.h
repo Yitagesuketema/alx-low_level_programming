0d4b4306a7811d536f72a421d2fc707165d3eaa9
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-09 13:08:23 +0300

    argument for 3-function_like_macro.h updated

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef ABS_FUNC_MACRO /* if ABS_FUNC_MACRO not defined */
 #define ABS_FUNC_MACRO /* define ABS_FUNC_MACRO */
-#define ABS(x) (x > 0 ? (x) : (x) * -1)
+#define ABS(x) ((x) > 0 ? (x) : (x) * -1)
 #endif /* end ABS_FUNC_MACRO */
