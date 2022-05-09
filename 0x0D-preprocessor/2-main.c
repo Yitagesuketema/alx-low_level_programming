09f2bd27089bbc31ef5f91655025e000544ed7c0
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-09 12:42:40 +0300

    main function updated

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/* main - entry point 
- * Return: Void.
+/* main -  prints the name of the file it was compiled
+ * Return: int (0);
  */
-int main()
+int main(void)
 {
 printf("%s\n",__FILE__);
 return (0);
