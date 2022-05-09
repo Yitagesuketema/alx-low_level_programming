61279bab975e91bec7ae5440398600854e215d9c
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-09 21:26:58 +0300

    betty style error fixed for 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,5 @@
-#ifndef ABS_FUNC_MACRO /* if ABS_FUNC_MACRO not defined */
-#define ABS_FUNC_MACRO /* define ABS_FUNC_MACRO */
-#define ABS(x) ((x) > 0 ? (x) : (x) * -1)
+#ifndef ABS_FUNC_MACRO
+#define ABS_FUNC_MACRO
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
 #endif /* end ABS_FUNC_MACRO */
+
