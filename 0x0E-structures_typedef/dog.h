64827d1802aaa60754e2d6413bb83fc601fd057e
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-09 23:41:05 +0300

    function prototype added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,5 +11,12 @@ struct  dog
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
