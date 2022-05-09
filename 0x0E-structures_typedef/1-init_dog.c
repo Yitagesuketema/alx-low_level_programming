79001b013bb8d3433282e28747fc3c4001eb5920
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-09 23:51:32 +0300

    trailling whitespace fixed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,15 +9,13 @@
  *@owner: owner
  * Description: This struct is for dog
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == NULL)
-return;
-else
-{
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
