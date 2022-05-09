c32f53177413efb295bdc93bdcb9407cb7e71e2f
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-09 22:03:55 +0300

    description added for dog.h header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct  dog
 {
-char * name;
+char *name;
 float age;
-char * owner;
+char *owner;
 };
+#endif
