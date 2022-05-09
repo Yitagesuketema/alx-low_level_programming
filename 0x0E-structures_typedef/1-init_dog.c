22fba9792eef643141a93f3eaef156ab12cde6a6
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-09 23:35:55 +0300

    initialize func struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,13 +12,5 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
-
-    else
-    {
-        d->name = name;
-        d->age = age;
-        d->owner = owner;
-    }
+   
 }
