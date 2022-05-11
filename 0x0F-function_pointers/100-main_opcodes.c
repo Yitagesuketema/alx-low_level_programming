36c31e3787107a6846b7885a4d4f97fbf7670253
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-11 22:37:22 +0300

    code for task 5 updated

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,28 +1,49 @@
-#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- *main -  prints the opcodes of its own main function.
- *@argc: integer value.
- *@argv: character value.
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
  *
- *Return: 0(success)
+ * Return: void
  */
-int main(int argc, char *argv[])
+void print_opcodes(char *a, int n)
 {
-	int i;
+int i;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	if (atoi(argv[1]) < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-	for (i = 0; i < atoi(argv[1]) - 1; i++)
-		printf("%02hhx ", ((char *)main)[i]);
-	printf("%02hhx\n", ((char *)main)[i]);
-	return (0);
+for (i = 0; i < n; i++)
+{
+printf("%.2hhx", a[i]);
+if (i < n - 1)
+printf(" ");
+}
+printf("\n");
+
+}
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ *
+ * Return: always O
+ */
+int main(int argc, char **argv)
+{
+int n;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+n = atoi(argv[1]);
+if (n < 0)
+{
+printf("Error\n");
+exit(2);
+}
+print_opcodes((char *)&main, n);
+return (0);
 }
