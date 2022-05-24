692e558cc03dd0fe1cc28d24c136d75be33c6cff
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-24 16:12:48 +0300

    lists header updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
 
 /**
@@ -28,5 +27,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* LISTS_H */
+
