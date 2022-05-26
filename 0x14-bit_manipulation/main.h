de680b4f66cdf660dde7f2da0b7da5f25cd70e35
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-05-26 20:56:15 +0300

    main header updated

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #define BYTE_LENGHT 8
 
 int _putchar(char c);
@@ -10,5 +9,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
+int get_endianness(void);
 #endif
